2823e753ba4e923664d8f956781a9ca1ccd0106a
Evige <jussi.heikkila1@gmail.com>
2015-05-04 21:00:54 +0300

    deleting emails from server

--- server/src/main/java/kala/alarm/server/service/EmailService.java
+++ server/src/main/java/kala/alarm/server/service/EmailService.java
@@ -8,6 +8,7 @@ import javax.inject.Singleton;
 import javax.ws.rs.Path;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 @Singleton
@@ -28,6 +29,16 @@ public class EmailService {
         LOG.debug("Email address {}, id: {}", email.getAddress(), email.getId());
         emails.add(email);
         return email;
+    }
+
+    public void deleteEmail(int id) {
+        Iterator<Email> i = emails.iterator();
+        while (i.hasNext()) {
+            Email email = i.next();
+            if (email.getId() == id) {
+                i.remove();
+            }
+        }
     }
 
     private int generateId() {
